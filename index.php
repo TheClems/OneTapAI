aec4704f7213f20c18a5be2bbe881925ef8af8dc
Clément TS <clement@thillay.net>
2025-06-15 01:05:18 +0200

    retour

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>All your AI models in one place - Try Pixa Playground</title>
         <meta name="description" content="Get all your AI models and tools in one place" />
         <link
             rel="shortcut icon"
@@ -12,7 +11,6 @@
         />
 
         <!-- Open Graph / Facebook -->
-        <meta property="og:title" content="All your AI models in one place - Try Pixa Playground" />
         <meta property="og:description" content="Get all your AI models and tools in one place" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="" />
@@ -64,6 +62,7 @@
                         class="tw-object-contain tw-h-full tw-w-full dark:tw-invert"
                     />
                 </div>
+                <span class="tw-text-base tw-font-medium">OneTapAI</span>
             </a>
             <div
                 class="collapsible-header animated-collapse max-lg:tw-shadow-md"
@@ -170,7 +169,6 @@
                     </button>
                     <a
                         href="#"
-                        aria-label="Try Pixa Playground"
                         class="btn tw-flex tw-gap-3 tw-px-3 tw-py-2 tw-transition-transform 
                                     tw-duration-[0.3s] hover:tw-translate-x-2"
                     >
@@ -208,7 +206,6 @@
                     <div class="tw-flex tw-w-full  tw-rounded-xl tw-px-[5%] max-md:tw-px-2 tw-min-h-[300px] tw-max-h-[90%] tw-h-full">
 
                         <div class="tw-relative tw-bg-black tw-min-w-full tw-min-h-full tw-overflow-clip tw-rounded-md">
-                            <!-- add your youtube video link here -->
                             <iframe class="tw-absolute tw-top-[50%] tw--translate-y-[50%] tw-left-[50%] tw--translate-x-[50%] tw-w-full tw-h-full" 
                                 src="https://www.youtube.com/embed/6j4fPVkA3EA?si=llcTrXPRM-MRXDZB&amp;controls=0&rel=0&showinfo=0&autoplay=1&loop=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                         </div>
@@ -238,8 +235,6 @@
                         class="reveal-up tw-mt-8 tw-max-w-[450px] tw-text-lg max-lg:tw-text-base tw-p-2 tw-text-center
                          tw-text-gray-800 dark:tw-text-white max-lg:tw-max-w-full"
                     >
-                        <!-- Test, Develop and Deploy all your AI models in one place, with Pixa seamless playground interface -->
-                        Your all in one AI companion. generate Images, videos, codes, docs, debug your web apps all with Pixa's interface.
                     </div>
 
                     <div
@@ -312,7 +307,6 @@
                                     </div>
 
                                     <div class="tw-mt-3 tw-text-lg">
-                                        Join Ben and 20,000+ users using Pixa
                                     </div>
 
                                     <a href="#" class="btn">
@@ -324,7 +318,6 @@
                                 <div class="tw-min-w-[250px] max-lg:tw-hidden tw-p-2 tw-gap-2 tw-flex tw-flex-col tw-bg-gray-100 
                                             dark:tw-bg-[#171717] tw-h-full">
                                     <!-- <h3 class="tw-text-xl tw-uppercase">
-                                        Pixa
                                     </h3> -->
                                     <div class="tw-h-[30px] tw-w-fit tw-max-w-[100px]">
                                         <img
@@ -370,11 +363,11 @@
 
                                 </div>
 
-                                <div class="tw-flex tw-w-full tw-p-4 tw-bg-white dark:tw-bg-black tw-h-full tw-flex-col" id="pixa-playground">
+                                <div class="tw-flex tw-w-full tw-p-4 tw-bg-white dark:tw-bg-black tw-h-full tw-flex-col" id="OneTapAI-playground">
+                                <div class="tw-flex tw-w-full tw-p-4 tw-bg-white dark:tw-bg-black tw-h-full tw-flex-col" id="OneTapAI-playground">
                                     <div class="tw-relative tw-w-full tw-flex tw-place-content-center tw-h-full">
                                         <div class="tw-absolute tw-top-[20%] max-lg:tw-top-[30%] tw-left-1/2 tw--translate-x-1/2  tw-w-[150px] tw-h-[150px]">
                                             <img src="./assets/logo/logo.png" class="tw-w-full tw-h-full dark:tw-invert tw-object-contain tw-opacity-20"
-                                                alt="Pixa logo">
                                         </div>
                                         <div class="prompt-container tw-overflow-y-auto tw-px-[5%] max-lg:tw-px-2 scrollbar max-lg:tw-max-h-[80%] tw-max-h-[550px] 
                                                     max-lg:tw-mt-12 tw-w-full tw-h-full tw-z-10 tw-flex tw-flex-col" id="prompt-container">
@@ -467,8 +460,6 @@
         <section
             class="tw-relative tw-flex tw-max-w-[100vw] tw-flex-col tw-place-content-center tw-place-items-center tw-overflow-hidden"
         >   
-
-
             <div
                 class="tw-mt-8 tw-flex tw-flex-col tw-w-full tw-h-full tw-place-items-center tw-gap-5"
             >
@@ -520,6 +511,7 @@
                                 </div>
                                 <h2 class="tw-text-3xl max-md:tw-text-2xl tw-font-medium">API Access</h2>
                                 <p class="tw-leading-normal tw-text-gray-800 dark:tw-text-gray-200">
+                                 OneTapAI's LLM API offers advanced summarization, text generation, and question-answering. Easily integrate with support for JSON, HTML, Markdown, and plain text, enhancing your applications with powerful language tools.
                                 </p>
                                 <div class="tw-flex tw-items-center tw-gap-2 tw-mt-auto">
                                     <span>Learn more</span>
@@ -539,6 +531,7 @@
                                 </div>
                                 <h2 class="tw-text-3xl max-md:tw-text-2xl tw-font-medium">Pre-built Tools</h2>
                                 <p class="tw-leading-normal tw-text-gray-800 dark:tw-text-gray-200">
+                                    OneTapAI offers pre-built AI integrations for diverse creative tasks including image, video, music, and PDF generation, simplifying advanced feature integration into your apps.
                                 </p>
                                 <div class="tw-flex tw-items-center tw-gap-2 tw-mt-auto">
                                     <span>Learn more</span>
@@ -560,6 +553,7 @@
                             <div class="tw-flex tw-flex-col tw-gap-4">
                                 <h2 class="tw-text-3xl max-md:tw-text-2xl tw-font-medium">Multiple AI models</h2>
                                 <p class="tw-leading-normal tw-text-gray-800 dark:tw-text-gray-200">
+                                OneTapAI supports various AI models, including ChatGPT, Gemini, Claude, Mistral and more, providing a range of advanced capabilities for various language and creative tasks.
                                 </p>
                                 <div class="tw-flex tw-items-center tw-gap-2 tw-mt-auto">
                                     <span>Learn more</span>
@@ -960,7 +954,6 @@
             <h3
                 class="reveal-up tw-text-4xl tw-font-medium tw-text-center max-md:tw-text-2xl"
             >
-                Join the professionals using Pixa
             </h3>
             <!-- Testimonials -->
             <div
@@ -1318,10 +1311,12 @@
                     <h4
                         class="faq-accordion tw-flex tw-w-full tw-select-none tw-text-xl max-md:tw-text-lg"
                     >
+                        <span>What's OneTapAI playground?</span>
                         <i class="bi bi-plus tw-text-xl tw-origin-center tw-duration-300 tw-transition-transform 
                                     tw-ml-auto tw-font-semibold"></i>
                     </h4>
                     <div class="content max-lg:tw-text-sm">
+                        OneTapAI playground is an integrated webapp to seamlessly test different LLM models such as GPT4, Claude, Gemini, etc.
                     </div>
                 </div>
                 <hr>
@@ -1351,6 +1346,7 @@
                                     tw-ml-auto tw-font-semibold"></i>
                     </h4>
                     <div class="content max-lg:tw-text-sm">
+                        You can use OneTapAI's AI Playground to test different models, including GPT4, Claude, Perplexity and more.
                     </div>
                 </div>
                 <hr>
@@ -1361,14 +1357,9 @@
                     <h4 class="faq-accordion tw-flex tw-w-full tw-select-none tw-text-xl max-md:tw-text-lg">
                         <span>Is OneTapAI free to use?</span>
                         <i class="bi bi-plus tw-text-xl tw-origin-center tw-duration-300 tw-transition-transform tw-ml-auto tw-font-semibold"></i>
-                    <h4
-                        class="faq-accordion tw-flex tw-w-full tw-select-none tw-text-xl max-md:tw-text-lg"
-                    >
-                        <span>Is Pixa Free to use?</span>
-                        <i class="bi bi-plus tw-text-xl tw-origin-center tw-duration-300 tw-transition-transform 
-                                    tw-ml-auto tw-font-semibold"></i>
                     </h4>
                     <div class="content max-lg:tw-text-sm">
+                        You can start using OneTapAI for free, and later upgrade your plan to access all its features.
                     </div>
                 </div>
                 <hr>
@@ -1417,26 +1408,22 @@
                             class="tw-max-w-[120px] dark:tw-invert"
                         />
                         <div class="tw-max-w-[120px] tw-text-center tw-text-3xl tw-h-fit">
-                            PIXA
                         </div>
                     </a>
                     <!-- <div class="tw-mt-3 tw-text-lg tw-font-semibold">Follow us</div> -->
                     <div class="tw-flex tw-gap-4 tw-text-lg">
                         <a
-                            href="https://github.com/PaulleDemon/"
                             aria-label="Github"
                         >
                             <i class="bi bi-github"></i>
                         </a>
                         <a
-                            href="https://twitter.com/pauls_freeman"
                             aria-label="Twitter"
                         >
                             <i class="bi bi-twitter"></i>
                         </a>
                       
                         <a
-                            href="https://www.linkedin.com/"
                             aria-label="Linkedin"
                         >
                             <i class="bi bi-linkedin"></i>
@@ -1463,8 +1450,6 @@
                         <div class="tw-flex tw-flex-col tw-gap-3">
                             <a href="#" class="footer-link">Support channels</a>
                             <a href="#" class="footer-link">Systems</a>
-                            <a href="https://twitter.com/pauls_freeman" class="footer-link">Twitter</a>
-                            <a href="https://github.com/PaulleDemon" class="footer-link">Github</a>
                         </div>
                     </div>
 
@@ -1482,7 +1467,6 @@
             <hr class="tw-mt-8">
             <div class="tw-mt-2 tw-flex tw-gap-2 tw-flex-col tw-text-gray-700 dark:tw-text-gray-300 tw-place-items-center 
                     tw-text-[12px] tw-w-full tw-text-center tw-place-content-around">
-                <span>Copyright &#169; 2023-2025</span>
                 <span>All trademarks and copyrights belong to their respective owners.</span>
             </div>
 
@@ -1507,4 +1491,4 @@
     <script src="./scripts/components.js"></script>
     <script src="./scripts/index.js"></script>
 
-</html></html>
+</html>
