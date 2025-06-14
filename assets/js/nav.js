27e049dff9639fd2398776ea922cce0ddd50b196
Gateaublabla <benjamin.icart@gmail.com>
2025-06-15 00:40:33 +0200

    Auto commit: modification sur assets\js\nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,21 +8,21 @@ const welcome = document.querySelector('.welcome');
 const infovalue = document.querySelector('.info-value');
 const infoCard = document.querySelector('.info-card');
 // Gestion du thème
+// Gestion du thème
 let isDarkMode = true;
+const elementsToToggle = [infovalue, infoCard, welcome];
 
 themeToggle.addEventListener('click', () => {
     isDarkMode = !isDarkMode;
     body.classList.toggle('light-mode');
-    infovalue.classList.toggle('light-mode');
-    infoCard.classList.toggle('light-mode');
-    welcome.classList.toggle('light-mode');
 
+    elementsToToggle.forEach(el => {
+        if (el) el.classList.toggle('light-mode');
+    });
 
     if (isDarkMode) {
-        // Icône lune pour le mode sombre
         themeIcon.innerHTML = '<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"/>';
     } else {
-        // Icône soleil pour le mode clair
         themeIcon.innerHTML = '<path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"/>';
     }
 });
