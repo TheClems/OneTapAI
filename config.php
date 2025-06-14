2df209fc4a7d73affbd55e5ffe6bdc7f1ce19144
Gateaublabla <benjamin.icart@gmail.com>
2025-06-14 17:31:25 +0200

    Auto commit: modification sur config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Configuration de la base de données
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u804247293_OneTapAI');
-define('DB_USER', 'u804247293_root');
+define('DB_NAME', '');
+define('DB_USER', '');
 define('DB_PASS', 'OcYYYC:3tM$'); // Mot de passe vide par défaut pour XAMPP
 
 // Connexion à la base de données
