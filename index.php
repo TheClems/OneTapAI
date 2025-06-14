a79a82932cc34378ad69ab168b33d2511f1399cb
Clément TS <clement@thillay.net>
2025-06-14 20:32:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
                         class="tw-object-contain tw-h-full tw-w-full dark:tw-invert"
                     />
                 </div>
-                <span class="tw-uppercase tw-text-base tw-font-medium">OneTap AI</span>
+                <span class="tw-text-base tw-font-medium">OneTapAI</span>
             </a>
             <div
                 class="collapsible-header animated-collapse max-lg:tw-shadow-md"
