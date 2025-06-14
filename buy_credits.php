29e39bfa83aaf65765576a4edc2bf73af4d08225
Gateaublabla <benjamin.icart@gmail.com>
2025-06-14 23:07:44 +0200

    Auto commit: modification sur buy_credits.php

--- buy_credits.php
+++ buy_credits.php
@@ -225,6 +225,14 @@ $packages = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <span class="nav-text">Paramètres</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="account.php" class="nav-link pulse-effect">
+                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"/>
+                    </svg>
+                    <span class="nav-text">Mon Compte</span>
+                </a>
+            </li>
         </ul>
     </nav>
     <div class="container">
