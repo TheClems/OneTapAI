b4f2661e0441ae6fcaec541162643c9d18ee5154
Gateaublabla <benjamin.icart@gmail.com>
2025-06-15 01:59:23 +0200

    Auto commit: modification sur auth2.php

--- auth2.php
+++ auth2.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once 'config.php';
-$passwd_required="required";
 
 $mode = isset($_GET['mode']) ? $_GET['mode'] : 'login';
 $error = '';
