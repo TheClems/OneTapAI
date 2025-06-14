4f4dc0a1ddf7e1712b877ed47fe69d1d12876637
Gateaublabla <benjamin.icart@gmail.com>
2025-06-15 01:30:58 +0200

    Auto commit: modification sur auth2.php

--- auth2.php
+++ auth2.php
@@ -188,12 +188,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			<div class="overlay-panel overlay-left">
 				<h1>Welcome Back!</h1>
 				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
+				<button class="ghost" id="signIn" onclick="window.location.href='auth2.php?mode=login'">Sign In</button>
 			</div>
 			<div class="overlay-panel overlay-right">
 				<h1>Hello, Friend!</h1>
 				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+				<button class="ghost" id="signUp" onclick="window.location.href='auth2.php?mode=register'">Sign Up</button>
 			</div>
 		</div>
 	</div>
