9da343a836e5efe0507582546df246bc1f28d850
Gateaublabla <benjamin.icart@gmail.com>
2025-06-15 22:59:37 +0200

    Auto commit: modification sur account.php

--- account.php
+++ account.php
@@ -13,79 +13,12 @@ $user = getCurrentUser();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Compte</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lucide/0.263.1/umd/lucide.js"></script>
-    <link rel="stylesheet" href="css/nav.css" />
     <link rel="stylesheet" href="css/account.css" />
 
 </head>
 <body>
+<?php require_once 'nav.php'; ?>
 
-    <nav class="sidebar" id="sidebar">
-        <div class="floating-particles" id="particles"></div>
-
-        <button class="toggle-btn" id="toggleBtn"></button>
-
-        <div class="nav-header">
-            <div class="logo">OneTapAI</div>
-            <button class="theme-toggle" id="themeToggle">
-                <svg class="theme-icon" id="themeIcon" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                </svg>
-            </button>
-        </div>
-
-        <ul class="nav-menu">
-            <li class="nav-item">
-                <a href="test.php" class="nav-link pulse-effect">
-                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                    </svg>
-                    <span class="nav-text">Accueil</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="test2.php" class="nav-link pulse-effect">
-                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span class="nav-text">Projets</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="gpt.php" class="nav-link pulse-effect">
-                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3z" />
-                    </svg>
-                    <span class="nav-text">Chat</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="dashboard.php" class="nav-link pulse-effect">
-                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
-                    </svg>
-                    <span class="nav-text">Dashboard</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="buy_credits.php" class="nav-link pulse-effect">
-                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                    </svg>
-                    <span class="nav-text">Acheter des crédits</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="account.php" class="nav-link pulse-effect">
-                    <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-                    </svg>
-                    <span class="nav-text">Mon Compte</span>
-                </a>
-            </li>
-
-        </ul>
-    </nav>
     <div class="floating-elements">
         <div class="floating-element"></div>
         <div class="floating-element"></div>
@@ -199,5 +132,4 @@ $user = getCurrentUser();
 </body>
 
 </html>
-<script type="text/javascript" src="scripts/nav.js"></script>
 <script type="text/javascript" src="scripts/account.js"></script>
