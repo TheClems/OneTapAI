75dceb6b3a8676eac062a032f8ab2e41162d6856
Gateaublabla <benjamin.icart@gmail.com>
2025-06-15 21:56:25 +0200

    Auto commit: modification sur account.php

--- account.php
+++ account.php
@@ -35,7 +35,7 @@ $user = getCurrentUser();
 
         <ul class="nav-menu">
             <li class="nav-item">
-                <a href="test.php" class="nav-link active pulse-effect">
+                <a href="test.php" class="nav-link pulse-effect">
                     <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
                     </svg>
