bcf0378853f412dffcd03d980d4a98c44bf5670a
Gateaublabla <benjamin.icart@gmail.com>
2025-06-16 23:40:56 +0200

    Auto commit: modification sur scripts\nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -33,7 +33,7 @@ const chatModel = document.querySelectorAll('.chat-model');
 const chatTime = document.querySelectorAll('.chat-time');
 const h3 = document.querySelectorAll('h3');
 const historyHeader = document.querySelectorAll('.history-header');
-const chatMessages = document.querySelector('.chat-messages');
+var chatMessages = document.querySelector('.chat-messages');
 
 const elementsToToggle = [
     ...infoValues,
