2753adeaad6cf8123120865237c03e863edf25b2
Gateaublabla <benjamin.icart@gmail.com>
2025-06-16 21:18:29 +0200

    Auto commit: modification sur nav.php

--- nav.php
+++ nav.php
@@ -46,7 +46,7 @@ $isDarkMode = $user['dark_mode'] == 1 ? 'true' : 'false';
             </a>
         </li>
         <li class="nav-item">
-            <a href="mistral_chat.php" class="nav-link pulse-effect">
+            <a href="chat.php" class="nav-link pulse-effect">
                 <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3z" />
                 </svg>
