2e6ebdf090365b0e2eb937bfaf6b94c0d1455aa0
Gateaublabla <benjamin.icart@gmail.com>
2025-06-17 23:07:33 +0200

    Auto commit: modification sur forgot_passwd.php

--- forgot_passwd.php
+++ forgot_passwd.php
@@ -173,34 +173,52 @@ if (isset($_GET['forgot']) && $_GET['forgot'] == 1) {
     <div class="alert alert-success" style="color: green; font-weight: bold; font-size: 16px; margin-bottom: 10px; text-align: center; padding: 10px; border-radius: 5px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);"><?php echo htmlspecialchars($success); ?></div>
 <?php endif; ?>
 
-<html><head><link rel="stylesheet" href="css/auth.css">
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/auth.css">
 
 
 
-</head><body><div class="container" id="container">
-    
-    <div class="form-container sign-in-container">
-        <form method="POST">
-            <h1>Forgot Password ?</h1>
-            
-            <span>Enter your email or username</span>
-            <input id="email" name="email" placeholder="Email" required="">
-            
-            
-            <button>Send code</button>
-        </form>
-    </div>
-    
-</div>
+</head>
 
-<script type="text/javascript" src="scripts/auth.js"></script>
-<script>
-    function delayedRedirect(url) {
-        setTimeout(function() {
-            window.location.href = url;
-        }, 800);
+<style>
+    .sign-in-container {
+        width: 100%;
+
     }
-</script></body></html>
+
+</style>
+
+
+<body>
+    <div class="container" id="container">
+
+        <div class="form-container sign-in-container">
+            <form method="POST">
+                <h1>Forgot Password ?</h1>
+
+                <span>Enter your email or username</span>
+                <input id="email" name="email" placeholder="Email" required="">
+
+
+                <button>Send code</button>
+            </form>
+        </div>
+
+    </div>
+
+    <script type="text/javascript" src="scripts/auth.js"></script>
+    <script>
+        function delayedRedirect(url) {
+            setTimeout(function() {
+                window.location.href = url;
+            }, 800);
+        }
+    </script>
+</body>
+
+</html>
 
 <script type="text/javascript" src="scripts/auth.js"></script>
 <script>
