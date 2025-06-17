9a1d589dd56563920eef90880c0e163649ce96ca
Gateaublabla <benjamin.icart@gmail.com>
2025-06-17 19:58:41 +0200

    Auto commit: modification sur projects.php

--- projects.php
+++ projects.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Métiers IA - Portfolio Professionnel</title>
+    <link rel="stylesheet" href="css/chat.css">
+
     <style>
         * {
             margin: 0;
