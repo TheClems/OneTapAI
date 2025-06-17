d505e22d58d64d03cb27b1df05679b93b1e1c59e
Gateaublabla <benjamin.icart@gmail.com>
2025-06-17 12:45:41 +0200

    Auto commit: modification sur gemini_api.php

--- gemini_api.php
+++ gemini_api.php
@@ -147,7 +147,7 @@ if (!$chatChannelId) {
 }
 
 // Clé API Gemini - Remplacez par votre vraie clé API
-$apiKey = 'VOTRE_CLE_API_GEMINI_ICI';
+$apiKey = 'AIzaSyCCi1o7eOxryROJIG26YS3vlR1tKC1dFcc';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     sendJsonResponse(['success' => false, 'error' => 'Méthode non autorisée'], 405);
