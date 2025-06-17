bd8624a7152aad89e7f04e51d59e58ecafd207a5
Gateaublabla <benjamin.icart@gmail.com>
2025-06-17 22:38:47 +0200

    Auto commit: modification sur addpersona.php

--- addpersona.php
+++ addpersona.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once 'config.php';
-getDBConnection();
+$pdo = getDBConnection();
 $message = '';
 
 // Traitement du formulaire
