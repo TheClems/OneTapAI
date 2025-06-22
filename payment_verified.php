d2d3547869b78fd41c49abb3abefffa8ff44f898
Gateaublabla <benjamin.icart@gmail.com>
2025-06-22 20:00:10 +0200

    Update payment_verified.php

--- payment_verified.php
+++ payment_verified.php
@@ -1,7 +1,14 @@
 <?php
-// Désactive les redirections automatiques
+// Évite toute redirection ou output
 ini_set('display_errors', 0);
 error_reporting(0);
+
+// Force la méthode POST seulement
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo "Method Not Allowed";
+    exit();
+}
 
 require 'vendor/autoload.php';
 
@@ -54,9 +61,19 @@ switch ($event->type) {
         $montant_total = $session->amount_total ?? 0;
         
         // Ici tu peux traiter les données (base de données, emails, etc.)
-        // Par exemple :
-        // updateUserSubscription($client_reference_id, $subscription_id);
-        // sendWelcomeEmail($customer_email);
+        // Exemple avec les données reçues :
+        // Customer ID : cus_SXxuE1i9QtiV6t
+        // Subscription ID : sub_1RcryXRpHQWEgzdpotkO4NbI  
+        // Email : gateaublabla.gateaublabla@gmail.com
+        // Client ref : 12
+        
+        // Log pour debug (supprime en production)
+        file_put_contents('webhook_success.txt', 
+            "SUCCESS - " . date('Y-m-d H:i:s') . 
+            " - Customer: " . $stripe_customer_id . 
+            " - Email: " . $customer_email . "\n", 
+            FILE_APPEND
+        );
         
         break;
         
