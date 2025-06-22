5542b03ccfbbc814a06425d9214729fe7be7c384
Gateaublabla <benjamin.icart@gmail.com>
2025-06-22 23:49:54 +0200

    Update payment_verified.php

--- payment_verified.php
+++ payment_verified.php
@@ -262,27 +262,3 @@ exit();
 ?>
 
 
-{
-    "timestamp": "2025-06-22 18:34:39",
-    "event_type": "checkout.session.completed",
-    "client_reference_id": "12",
-    "customer_email": "gateaublabla.gateaublabla@gmail.com",
-    "customer_id": "cus_SXy93YbKcMw5FA",
-    "subscription_id": "sub_1RcsD0RpHQWEgzdpvZ1TNBLi",
-    "montant_total": 2900,
-    "produits_achetes": [
-        {
-            "price_id": "price_1RcopTRpHQWEgzdpPM2GBk4A",
-            "product_id": "prod_SXueUi0jM0ghuH",
-            "product_name": "Professionnal",
-            "product_description": "400 000 cr\u00e9dits par mois, acc\u00e8s aux meilleurs LLM, g\u00e9n\u00e9ration d'image, acc\u00e8s aux personas, customisation des personas et acc\u00e8s en avance aux nouvelles fonctionnalit\u00e9s.",
-            "quantity": 1,
-            "amount_total": 2900,
-            "currency": "eur",
-            "metadata": [],
-            "subscription_item_id": "si_SXy9nHqhWGXvAJ",
-            "subscription_interval": "month"
-        }
-    ]
-}
-
