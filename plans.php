648764e1330ab6092d148f5a8e0906943599a258
Clément TS <clement@thillay.net>
2025-06-22 23:13:46 +0200

    Update plans.php

--- plans.php
+++ plans.php
@@ -48,9 +48,11 @@ $user = getCurrentUser();
                 <hr />
                 <ul>
                     <li><i class="bi bi-check-circle-fill"></i><span>100 000 Crédits</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Accès aux meilleurs LLM</span></li>
                     <li><i class="bi bi-check-circle-fill"></i><span>&#126; 2 000 messages de chat IA</span></li>
-                    <li><i class="bi bi-check-circle-fill"></i><span>&#126; 50 générations d'images premium</span></li>
-                    <li class="disabled"><i class="bi bi-check-circle-fill"></i><span>Accès à tous les modèles IA premium</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>&#126; 50 générations d'images </span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Accès aux Personas</span></li>
+                    <li class="disabled"><i class="bi bi-check-circle-fill"></i><span>Personalisation de Personas</span></li>
                     <li class="disabled"><i class="bi bi-check-circle-fill"></i><span>Accès anticipé aux nouvelles fonctionnalités</span></li>
                 </ul>
                 <a href="#" class="btn">Choisir ce plan</a>
@@ -65,10 +67,12 @@ $user = getCurrentUser();
                 <p>Fonctionnalités avancées pour les passionnés d'IA</p>
                 <hr />
                 <ul>
-                    <li><i class="bi bi-check-circle-fill"></i><span>200 000 Crédits</span></li>
+                <li><i class="bi bi-check-circle-fill"></i><span>200 000 Crédits</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Accès aux meilleurs LLM</span></li>
                     <li><i class="bi bi-check-circle-fill"></i><span>&#126; 4 000 messages de chat IA</span></li>
-                    <li><i class="bi bi-check-circle-fill"></i><span>&#126; 100 générations d'images premium</span></li>
-                    <li><i class="bi bi-check-circle-fill"></i><span>Accès à tous les modèles IA premium</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>&#126; 100 générations d'images </span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Accès aux Personas</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Personalisation de Personas</span></li>
                     <li class="disabled"><i class="bi bi-check-circle-fill"></i><span>Accès anticipé aux nouvelles fonctionnalités</span></li>
                 </ul>
                 <a href="#" class="btn">Choisir ce plan</a>
@@ -83,10 +87,12 @@ $user = getCurrentUser();
                 <p>Potentiel illimité pour les utilisateurs experts</p>
                 <hr />
                 <ul>
-                    <li><i class="bi bi-check-circle-fill"></i><span>400 000 Crédits</span></li>
+                <li><i class="bi bi-check-circle-fill"></i><span>400 000 Crédits</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Accès aux meilleurs LLM</span></li>
                     <li><i class="bi bi-check-circle-fill"></i><span>&#126; 8 000 messages de chat IA</span></li>
-                    <li><i class="bi bi-check-circle-fill"></i><span>&#126; 200 générations d'images premium</span></li>
-                    <li><i class="bi bi-check-circle-fill"></i><span>Accès à tous les modèles IA premium</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>&#126; 200 générations d'images </span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Accès aux Personas</span></li>
+                    <li><i class="bi bi-check-circle-fill"></i><span>Personalisation de Personas</span></li>
                     <li><i class="bi bi-check-circle-fill"></i><span>Accès anticipé aux nouvelles fonctionnalités</span></li>
                 </ul>
                 <a href="#" class="btn">Choisir ce plan</a>
