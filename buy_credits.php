00c39ea49cdd098267720620c2e1369a49c98e23
Clément TS <clement@thillay.net>
2025-06-23 14:35:44 +0200

    Update buy_credits.php

--- buy_credits.php
+++ buy_credits.php
@@ -34,8 +34,8 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/buy_credits.css" />
     <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="css/buy_credits.css" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Buy credits - OneTapAI</title>
 </head>
